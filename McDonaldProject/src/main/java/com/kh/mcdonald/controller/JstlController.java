b79159eeae61bdc3cbee4fffea76609f5c7ec2fe
Ara Jo <125568190+whgdkfk@users.noreply.github.com>
2025-03-12 17:41:19 +0900

    20250312 로그인,로그아웃

--- McDonaldProject/src/main/java/com/kh/mcdonald/controller/JstlController.java
+++ McDonaldProject/src/main/java/com/kh/mcdonald/controller/JstlController.java
@@ -16,27 +16,29 @@ import com.kh.mcdonald.model.service.UserService;
 public class JstlController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    public JstlController() {
-        super();
-    }
+	public JstlController() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		int point = 500;
 		request.setAttribute("point", point);
-		
-		String[] colors = {"red", "orangered", "orange", "yellow", "yellowgreen", "greenyellow", "green"};
+
+		String[] colors = { "red", "orangered", "orange", "yellow", "yellowgreen", "greenyellow", "green" };
 		request.setAttribute("colors", colors);
-		
+
 		// TB_USER 가지고 할 것
 		List<UserDTO> list = new UserService().findAll();
-		
+
 		request.setAttribute("users", list);
 		request.setAttribute("msg", "리스트가 널이 아니면 조회 성공");
 		request.getRequestDispatcher("/WEB-INF/views/jstl/JSTL.jsp").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
---
+++ MyFirstWebProject/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome File</title>
+</head>
+<body>
+	<!--  
+		* WEB 환경에서의 CRUD를 구현
+		* 회원서비스
+		  로그인(R), 회원가입(C)
+	-->
+
+	<jsp:include page="WEB-INF/views/include/header.jsp"/>
+	<jsp:include page="WEB-INF/views/include/main.jsp"/>
+	<jsp:include page="WEB-INF/views/include/footer.jsp"/>
+</body>
+</html>
